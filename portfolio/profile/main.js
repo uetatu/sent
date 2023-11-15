f51d8b304f87862df0b1fa07481f8a47ae7f37b2
uetatu <140932826+uetatu@users.noreply.github.com>
2023-11-15 23:41:48 +0900

    Delete portfolio/profile directory

--- portfolio/profile/main.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
-      $('.menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
-    })
-  });
