d747ccc3140bd6b7698ad34db5ebd8ea1cb9bd72
uetatu <140932826+uetatu@users.noreply.github.com>
2023-12-17 12:04:18 +0900

    Add files via upload

---
+++ Apple/js/main.js
@@ -0,0 +1,19 @@
+function updateOpacity() {
+    // Find the currently active slide
+    const activeSlide = document.querySelector('.slides-container .slide.active');
+  
+    // Get the next slide to be displayed
+    let nextSlide = activeSlide.nextElementSibling;
+  
+    if (!nextSlide) {
+      // If there is no next sibling, go back to the first slide
+      nextSlide = document.querySelector('.slides-container .slide:first-child');
+    }
+  
+    // Update opacity
+    activeSlide.classList.remove('active');
+    nextSlide.classList.add('active');
+  }
+  
+  // Update opacity every 5 seconds
+  setInterval(updateOpacity, 5000);
